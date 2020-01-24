e160f8e6ac5cf5f81232e3665488b9f4d1abae13
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-01-23 22:11:06 -0300

    Habilitar o plugin VueSignalR

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueSignalR from "./plugins/vue-signalR";
 
 Vue.config.productionTip = false
+Vue.use(VueSignalR);
 
 new Vue({
   render: h => h(App),
