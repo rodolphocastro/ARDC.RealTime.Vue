95ea703ed4a9e92c839e755149f2e1295bcbb0fb
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-01-23 21:41:01 -0300

    Realizar conexão inicial com o SignalR

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+const signalR = require("@microsoft/signalr");
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  mounted() {
+    let connection = new signalR.HubConnectionBuilder()
+      .withUrl("http://localhost/broadcastHub")
+      .build();
+
+    connection.start().then(console.log("Connected"));
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
