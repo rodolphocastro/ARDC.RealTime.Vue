422665fdc936153f92b6f138f9e6d9ca60984d80
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-01-23 22:40:56 -0300

    Importar o mini-default à raiz do app

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueSignalR from "./plugins/vue-signalR";
+import 'mini.css/dist/mini-default.min.css';
 
 Vue.config.productionTip = false
 Vue.use(VueSignalR);
