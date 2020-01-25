27aae6cf9d4a83dc085deece029719ce7a48ec0c
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-01-24 21:21:36 -0300

    Remover referência ao Component HelloWorld

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <div id="app" class="container">
-    <img alt="Vue logo" src="./assets/logo.png" class="row" />
-    <HelloWorld msg="Welcome to Your Vue.js App" class="row" />
+  <div>
+    <header class="sticky row">
+      <div class="col-sm-12 col-md-12 col-lg-10 col-lg-offset-1">
+        <a href="#" class="logo">ARDC Realtime</a>
+        <a href="index.html" class="button">Home</a>
+        <a href="https://github.com/rodolphocastro" class="button">My GitHub</a>
+      </div>
+    </header>
+    <div class="container">
+      <!-- TODO: Adicionar conteúdo do App -->
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "app",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
 
